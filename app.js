cdd8b3767d6591b3f593f8298d1dd66929e34743
TousifullaSharif <89535903+TousifullaSharif@users.noreply.github.com>
2023-08-25 16:03:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+const port = 3000
+
+const uri = "mongodb+srv://tousif:tousifulla@t-cluster.7yoyhjy.mongodb.net/?retryWrites=true&w=majority"
+
+  async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+connect();
+
+app.listen(port, () => {
+    console.log('App Server Started on port 3000 ')
+});
